a16d59caf1d4be9b40ff1f57cca9f964a70f8314
karim <karim-bahri-18@outlook.fr>
2021-10-20 14:08:20 +0100

    setting up server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config({ path: `${__dirname}/config.env` });
 const app = express();
 const port = process.env.PORT || 5000;
+
+// app.use(morgan("dev")); // development mode
 app.listen(port, () => {
   console.log(`Listenning to server in port: ${port}`);
 });
