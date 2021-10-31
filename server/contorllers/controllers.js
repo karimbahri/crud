896b44840fd3773fd6ec4d5b2d30955831b00f43
karim <karim-bahri-18@outlook.fr>
2021-10-31 22:51:32 +0100

    set controllers

---
+++ server/contorllers/controllers.js
@@ -0,0 +1,128 @@
+const userModel = require('../model/model');
+
+exports.createUser = (req, res) => {
+    if (!req.body) {
+        return res
+        .status(404)
+        .json({
+            status: 'Failure',
+            message: 'Cannot post empty object'
+        })
+    }
+    const user = new userModel({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    });
+
+    user
+    .save()
+    .then(data => {
+        res
+          .status(200)
+          .json({
+              status: 'Success',
+              data
+          })
+
+    })
+    .catch(err => {
+        res
+          .status(404)
+          .json({
+            status: 'Failure',
+            message: err.message || 'Error during user creation'
+          })
+    })
+}
+exports.findUser = (req, res) => {
+    userModel
+      .find()
+      .then(data => {
+          res
+            .status(200)
+            .json({
+                status: 'Success',
+                length: data.length,
+                data
+            })
+      })
+      .catch(err => {
+        res
+          .status(404)
+          .json({
+            status: 'Failure',
+            message: err.message || 'Error during finding users'
+          })
+        })
+}
+
+exports.updateUser = (req, res) => {
+    if (!req.body) {
+        return res
+          .status(404)
+          .json({
+            status: 'Failure',
+            message: 'Cannot put empty object'
+        })
+    }
+    userModel
+      .findByIdAndUpdate(req.params.id, req.body, {useFindAndModify: false})
+      .then(data => {
+          if (!data) {
+            return res
+            .status(400)
+            .json({
+              status: 'Failure',
+              message: 'Cannot update user! User not found'
+          })
+          }
+          else {
+            res
+              .status(200)
+              .json({
+                status: 'Success',
+                data
+            })
+          }
+      })
+      .catch(err => {
+        return res
+        .status(400)
+        .json({
+            status: 'Failure',
+            message: err.message || 'Error during updating user'
+        })
+    })
+}
+exports.deleteUser = (req, res) => {
+    userModel
+      .findByIdAndDelete(req.params.id)
+      .then(data => {
+          if (data) {
+              res
+                .status(200)
+                .json({
+                    status: 'success',
+                    data
+                })
+          }
+          else {
+              res
+                .status(400)
+                .json({
+                    status: 'failure',
+                    message: 'Cannot delete user! user not found'
+                })
+          }
+      })
+      .catch(err => {
+          res
+            .status(400)
+            .json({
+                status: 'failure',
+                message: err.message
+            })
+      })
+}
