87619f08b69b00a7eb0baabcaef9c98a549d0c2d
karim <karim-bahri-18@outlook.fr>
2021-10-31 22:50:08 +0100

    routing: udate/delete/find/create

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,17 @@
 const routes = require('express').Router();
 const routesControllers = require('../contorllers/render');
-
+const apiControllers = require('../contorllers/controllers');
 
 routes
   .get("/", routesControllers.home_page)
   .get("/add-user", routesControllers.add_user)
   .get("/update-user", routesControllers.update_user);
+
+// api crud requests handling
+routes
+  .post('/api/users', apiControllers.createUser)
+  .get('/api/users', apiControllers.findUser)
+  .put('/api/users/:id', apiControllers.updateUser)
+  .delete('/api/users/:id', apiControllers.deleteUser)
 
 module.exports = routes;
