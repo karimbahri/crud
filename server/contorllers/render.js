0b9dac619b5ceaf0a96a4e2ee7f0e7c3423a6e2f
karim <karim-bahri-18@outlook.fr>
2021-11-01 00:56:04 +0100

    render new users

--- server/contorllers/render.js
+++ server/contorllers/render.js
@@ -1,7 +1,14 @@
-
+const axios = require('axios');
 
 exports.home_page = (req, res) => {
-    res.render("index");
+    axios
+      .get('http://127.0.0.1:8080/api/users')
+        .then(usrs => {
+          res.render("index", { users: usrs.data.data});
+        })
+        .catch(err => {
+          res.send(err);
+        })
   }
 
 exports.add_user = (req, res) => {
